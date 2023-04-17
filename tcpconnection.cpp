631e713bd461205f680de5b52c77b9d48d9a09df
Jakub <jakubkontrik@gmail.com>
2023-04-17 09:56:37 +0200

    Add sigint handler for tcp module

--- tcpconnection.cpp
+++ tcpconnection.cpp
@@ -12,11 +12,20 @@
 #include <vector>
 #include <sstream>
 #include "parser.h"
+#include <mutex>
+
+std::mutex mtx;
 
 #define BUFFER_SIZE 1025
-
+struct thread_args {
+    int client_socket;
+    std::vector<int>* client_sockets_ptr;
+};
+struct thread_args *args;
 
 TCPConnection::TCPConnection(const char* host, int port){
+    this->comm_addr={};
+    this->comm_addr_size = sizeof(this->comm_addr);
     if((this->welcome_socket = socket(AF_INET,SOCK_STREAM,0))<0){
         perror("socket");
         exit(1);
@@ -44,34 +53,62 @@ void TCPConnection::listen_tcp(){
         perror("ERROR: listen");
         exit(EXIT_FAILURE); 
     }
+    std::vector<pthread_t> threads;
     while(1){
+        pthread_t thread;
         int communication_socket = accept(this->welcome_socket, this->comm_addr, &(this->comm_addr_size));
-        std::cout << "Accepted connection" << std::endl;
-        if (communication_socket < 0)
+        if (communication_socket < 0 and !sigint_flag)
         {
             perror("ERROR: accept");
             exit(EXIT_FAILURE);
         }
-        pthread_t thread;
-        int *client_socket_ptr = new int;
-        *client_socket_ptr = communication_socket;
-        pthread_create(&thread, NULL, handle_connection, (void*)client_socket_ptr);
+        if (sigint_flag){
+            std::cout << "Closing server" << std::endl;
+            for (int i = 0; i < this->client_sockets.size(); i++){
+                send(this->client_sockets[i], "BYE\n", 4, 0);
+                shutdown(this->client_sockets[i], SHUT_RDWR);
+                close(this->client_sockets[i]);
 
+            }
+            break;
+        }
+        this->client_sockets.push_back(communication_socket);
+        std::cout << "Accepted connection" << std::endl;
+        args = new thread_args;
+        args->client_socket = communication_socket;
+        args->client_sockets_ptr = &this->client_sockets;
+        pthread_create(&thread, NULL, handle_connection, (void*)args);
+        threads.push_back(thread);
+    }
+    std::cout << "Closing server" << std::endl;
+    for (auto &thread : threads) {
+        pthread_join(thread, NULL);
     }
 
 }
 
 void *handle_connection(void *arg){
     char buffer[BUFFER_SIZE];
-    int *client_socket_ptr = (int*)arg;
-    int client_socket = *client_socket_ptr;
-    delete client_socket_ptr;
+    thread_args *args = (thread_args*)arg;
+    int client_socket = args->client_socket;
+    std::vector<int>* client_sockets_ptr = args->client_sockets_ptr;
+    
+    delete args;
     int send_l;
     bzero(buffer,BUFFER_SIZE);
     std::cout << "Waiting for HELLO" << std::endl;
     send_l = recv(client_socket, buffer, BUFFER_SIZE-1, 0);
     if (send_l <= 0){
         perror("recvfrom:");
+        mtx.lock();
+        for (auto socket = client_sockets_ptr->begin(); socket != client_sockets_ptr->end(); socket++) {
+            if (*socket == client_socket) {
+                client_sockets_ptr->erase(socket);
+                break;
+            }
+        }
+        mtx.unlock();
+        std::cout << client_socket << std::endl;
         close(client_socket);
         pthread_exit(NULL);
     }
@@ -83,6 +120,14 @@ void *handle_connection(void *arg){
     }
     else{
         std::cout << "Invalid message" << std::endl;
+        mtx.lock();
+        for (auto socket = client_sockets_ptr->begin(); socket != client_sockets_ptr->end(); socket++) {
+            if (*socket == client_socket) {
+                client_sockets_ptr->erase(socket);
+                break;
+            }
+        }
+        mtx.unlock();
         send(client_socket, "BYE\n",4, 0);
         close(client_socket);
         pthread_exit(NULL);
@@ -92,22 +137,26 @@ void *handle_connection(void *arg){
         send_l = recv(client_socket, buffer, BUFFER_SIZE, 0);
         std::cout << "Received message: " << buffer;
         if (send_l <= 0){
-            perror("ERROR: recvfrom:");
+            perror("recvfrom:");
+            mtx.lock();
+            for (auto socket = client_sockets_ptr->begin(); socket != client_sockets_ptr->end(); socket++) {
+                if (*socket == client_socket) {
+                    client_sockets_ptr->erase(socket);
+                    break;
+                }
+            }
+            mtx.unlock();
             close(client_socket);
             pthread_exit(NULL);
         }
         std::string message(buffer);
         if (message == "BYE\n"){
-            send(client_socket, "BYE\n", 4, 0);
-            close(client_socket);
-            pthread_exit(NULL);
+            break;
         }
         size_t pos = message.find("SOLVE ");
         if (pos == std::string::npos or pos != 0){
             std::cout << "Invalid message" << std::endl;
-            send(client_socket, "BYE\n", 4, 0);
-            close(client_socket);
-            pthread_exit(NULL);
+            break;
         }
         message.erase(0,6);
         message.pop_back();
@@ -117,18 +166,36 @@ void *handle_connection(void *arg){
         }
         catch (...){
             std::cout << "Invalid message" << std::endl;
-            send(client_socket, "BYE\n",4, 0);
-            close(client_socket);
-            pthread_exit(NULL);
+            break;
         }
         message = "RESULT " + std::to_string(parser.result) + "\n";
         if (send(client_socket, message.c_str(), message.length(), 0) < 0){
             perror("ERROR: sendto:");
+            mtx.lock();
+            for (auto socket = client_sockets_ptr->begin(); socket != client_sockets_ptr->end(); socket++) {
+                if (*socket == client_socket) {
+                    client_sockets_ptr->erase(socket);
+                    break;
+                }
+            }
+            mtx.unlock();
             close(client_socket);
             pthread_exit(NULL);
         }
         bzero(buffer,BUFFER_SIZE);
     }
+    
+    mtx.lock();
+    for (auto socket = client_sockets_ptr->begin(); socket != client_sockets_ptr->end(); socket++) {
+        if (*socket == client_socket) {
+            client_sockets_ptr->erase(socket);
+            break;
+        }
+    }
+    
+    // unlock the mutex after modifying the vector
+    mtx.unlock();
+    send(client_socket, "BYE\n", 4, 0);
     close(client_socket);
     pthread_exit(NULL);
 }
--- tcpconnection.h
+++ tcpconnection.h
@@ -2,18 +2,19 @@
 #define TCPCONNECTION_H
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <vector>
 
-
+extern bool sigint_flag;
 class TCPConnection{
     private:
     int welcome_socket;
     struct sockaddr_in server_address;
     struct sockaddr *comm_addr;
     socklen_t comm_addr_size;
-    
 
 
     public:
+    std::vector<int> client_sockets;
     TCPConnection(const char* host, int port);
     void listen_tcp();
 };
